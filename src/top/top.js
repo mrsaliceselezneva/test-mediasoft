9ab56bc6593207bf383a4f0b4f42fb8757490329
mrsaliceselezneva <poluvesov.artyom@gmail.com>
2022-06-28 13:09:22 +0400

    добавлин top(header) с лого без поисковика

---
+++ src/top/top.js
@@ -0,0 +1,16 @@
+import './top.scss';
+
+function Top() {
+  return (
+    <header class="main-header">
+    <div class="full-width full-width--gutter">
+  
+      <a href="/" class="logo">
+        <img class="logo-shadow" src="https://developers.teleport.org/assets/logo.e663724a.svg" alt="Teleport" />
+      </a>
+    </div>
+  </header>
+  );
+}
+
+export default Top;
